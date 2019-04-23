ed1ab62ed2eca975777fd12c017c94b767907e0f
jhan_ty29 <jhancarloty@gmail.com>
2019-04-23 17:01:03 -0400

    inicio del sistema v1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel SISTEMA DE HOTEL SANTA ROSA 
                 </div>
 
                 <div class="links">
